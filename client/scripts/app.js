a10753ce904cdaef7bc10660d61ca29deee8e96d
Hack Reactor Student <student@hackreactor.com>
2017-05-26 17:39:46 -0700

    partially complete handleUserNameClick

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,13 +1,17 @@
 // YOUR CODE HERE:
+
 var app = {};
 
 app.init = function() {
+  this.server = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages';
+  this.handleUsernameClick();
+  this.friends = [];
 };
 
 app.send = function(message) {
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
-    url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
     type: 'POST',
     data: JSON.stringify(message),
     contentType: 'application/json',
@@ -21,3 +25,48 @@ app.send = function(message) {
   });
 };
 
+app.fetch = function() {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    // data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message received');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to receive message', data);
+    }
+  });
+};
+
+app.clearMessages = function() {
+  var allChats = $('#chats').children();
+  for (var i = 0; i < allChats.length; i++) {
+    allChats[i].remove();
+  }
+  // _.every(allChats, chat => chat.remove());
+};
+
+app.renderMessage = function(message) {
+  var textMessage = message.text;
+  var username = message.username;
+  // $('#main').append(`<div id='username'></div>`);
+  $('#chats').append(`<ol class="username" class="${username}">${username}:\n${textMessage}</ol>`);
+};
+
+app.renderRoom = function(roomName) {
+  $('#roomSelect').append(`<ul>${roomName}</ul>`);
+};
+
+app.handleUsernameClick = function() {
+  $('.username').click(function() {
+    console.log('WOOT');
+    this.friends.push.bind(this, 'asdf');
+  });
+};
+
+// server
+// underscore not working
